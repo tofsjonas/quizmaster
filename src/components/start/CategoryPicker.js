094e8753812190f8a281110299322eba6acf8c68
Your Name <you@example.com>
2020-06-20 18:04:56 +0200

    lighthouse optimizations

--- src/components/start/CategoryPicker.js
+++ src/components/start/CategoryPicker.js
@@ -1,20 +1,19 @@
-import React, { useContext } from 'react'
-import { SettingsContext } from 'contexts/SettingsContext'
+import React, { useContext } from "react";
+import { SettingsContext } from "contexts/SettingsContext";
 const CategoryPicker = () => {
-  const { category, setCategory } = useContext(SettingsContext)
-  const handleChange = e => {
-    setCategory(e.target.value)
-  }
+  const { category, setCategory } = useContext(SettingsContext);
+  const handleChange = (e) => {
+    setCategory(e.target.value);
+  };
 
   return (
     <div className="CategoryPicker">
-      <label>Category:</label>
-
-      <select onChange={handleChange} value={category}>
+      <label for="category-picker">Category:</label>
+      <select id="category-picker" onChange={handleChange} value={category}>
         <option value="10">Books</option>
         <option value="11">Movies</option>
       </select>
     </div>
-  )
-}
-export default CategoryPicker
+  );
+};
+export default CategoryPicker;
--- src/components/start/NumberOfQuestionsPicker.js
+++ src/components/start/NumberOfQuestionsPicker.js
@@ -1,22 +1,28 @@
-import React, { useContext } from 'react'
-import { SettingsContext } from 'contexts/SettingsContext'
+import React, { useContext } from "react";
+import { SettingsContext } from "contexts/SettingsContext";
 
 const NumberOfQuestionsPicker = () => {
-  const { numberOfQuestions, setNumberOfQuestions } = useContext(SettingsContext)
+  const { numberOfQuestions, setNumberOfQuestions } = useContext(
+    SettingsContext
+  );
 
-  const handleChange = e => {
-    setNumberOfQuestions(e.target.value)
-  }
+  const handleChange = (e) => {
+    setNumberOfQuestions(e.target.value);
+  };
 
   return (
     <div className="NumberOfQuestionsPicker">
-      <label>Number of Questions:</label>
-      <select value={numberOfQuestions} onChange={handleChange}>
+      <label for="number-of-questions-picker">Number of Questions:</label>
+      <select
+        id="number-of-questions-picker"
+        value={numberOfQuestions}
+        onChange={handleChange}
+      >
         <option value="5">5</option>
         <option value="10">10</option>
         <option value="15">15</option>
       </select>
     </div>
-  )
-}
-export default NumberOfQuestionsPicker
+  );
+};
+export default NumberOfQuestionsPicker;
