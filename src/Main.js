e6f6eaef6e538d6a35760aaf67594d8bf86d4d31
Your Name <you@example.com>
2020-06-20 18:17:39 +0200

    lazy components

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,24 @@
-import React, { useContext } from 'react'
-import Start from 'components/Start'
-import CountDown from 'components/start/CountDown'
-import Game from 'components/Game'
-import Statistics from 'components/Statistics'
-import { GameContext } from 'contexts/GameContext'
+import React, { useContext, lazy } from "react";
+import Start from "components/Start";
+import { GameContext } from "contexts/GameContext";
+const Game = lazy(() => import("components/Game"));
+const CountDown = lazy(() => import("components/start/CountDown"));
+const Statistics = lazy(() => import("components/Statistics"));
+
 const Main = () => {
-  const { counter, questions, current } = useContext(GameContext)
+  const { counter, questions, current } = useContext(GameContext);
 
   return (
     <main>
       {counter > 0 && <CountDown />}
       {counter === 0 && questions.length === 0 && <Start />}
-      {counter === 0 && questions.length > 0 && questions.length > current && <Game />}
-      {counter === 0 && questions.length > 0 && questions.length === current && <Statistics />}
+      {counter === 0 && questions.length > 0 && questions.length > current && (
+        <Game />
+      )}
+      {counter === 0 &&
+        questions.length > 0 &&
+        questions.length === current && <Statistics />}
     </main>
-  )
-}
-export default Main
+  );
+};
+export default Main;
